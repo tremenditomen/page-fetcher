90c8f8358e1fda86714c81979ad27ba71f19082a
Luis Andres Contreras <tremenditomen@gmail.com>
2021-09-22 16:00:56 -0600

    seting up the files

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+const { myNumber, myString, myFunction } = require("./client");
+
+// establishes a connection with the game server
+
+
+// const connect = function () {
+//     const conn = net.createConnection({
+//       host: '135.23.223.133',
+//       port: 50542
+//     });
+  
+//     // interpret incoming data as text
+//     conn.setEncoding("utf8");
+//     conn.on('data', (data)=>{
+  
+//       console.log(data)
+//     })
+//     return conn;
+// };
+
+// console.log("Connecting ...");
+// connect();
